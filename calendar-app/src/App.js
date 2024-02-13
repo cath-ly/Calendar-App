cf79e1cd1d09ea9c093467e2d354e62eb7ee395c
cath-ly <takath339@gmail.com>
2024-02-13 10:08:56 -0500

    Create placeholders for CalendarHeader, SideBar, and Month objects

--- calendar-app/src/App.js
+++ calendar-app/src/App.js
@@ -1,25 +1,19 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { getMonth } from '../components/util.js'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div className="h-screen flex flex-columns">
+        <CalendarHeader />
+        <div className="flex flex-1">
+          <SideBar />
+          <Month />
+        </div>
+      </div>
+    </React.Fragment>
   );
 }
 
