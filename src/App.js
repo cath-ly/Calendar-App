9fc7d54c200c5d43ab23c5c2d0e6b795abdcbd8c
cath-ly <takath339@gmail.com>
2024-02-19 19:54:16 -0500

    Added effects for handling months in the calendar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React, { useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import './App.css';
 
 import getMonth from './utils/getMonth.js'
 import CalendarHeader from './components/CalendarHeader.js';
 import SideBar from './components/SideBar.js';
 import Month from './components/Month.js';
+import GlobalContext from './contexts/GlobalContext.js';
 
 function App() {
   const [currentMonth, setCurrentMonth] = useState(getMonth())
+  const { monthIndex } = useContext(GlobalContext);
+
+  useEffect(() =>  {
+    setCurrentMonth(getMonth(monthIndex));
+  }, [monthIndex]);
   
   return (
     <React.Fragment>
--- src/components/CalendarHeader.js
+++ src/components/CalendarHeader.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import logo from '../assets/logo.png'
+import GlobalContext from '../contexts/GlobalContext'
+import dayjs from 'dayjs';
 
 //using rfc creates this template
 export default function CalendarHeader() {
+    const handlePrevMonth = function() {
+    setMonthIndex(monthIndex - 1);
+  };
+
+  const handleNextMonth = function(){
+    setMonthIndex(monthIndex + 1);
+  };
+
+  const { monthIndex, setMonthIndex } = useContext(GlobalContext);
   return (
     <header className="px-4 py-2 flex items-center">
         <img src={logo} alt="calendar" className="mr-2 w-12 h-12" />
@@ -12,16 +23,21 @@ export default function CalendarHeader() {
         <button className="border rounded py-2 px-4 mr-5">
           Today
         </button>
-        <button>
+        <button onClick={handlePrevMonth}>
           <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
             chevron_left
           </span>
         </button>
-        <button>
+        <button onClick={handleNextMonth}>
           <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
             chevron_right
           </span>
         </button>
+        <h2 className="ml-4 text-xl text-gray-500 font-bold">
+          {dayjs(new Date(dayjs().year(), monthIndex)).format(
+            "MMMM YYYY"
+            )}
+        </h2>
     </header>
   )
 }
