ece133a42640950c1db606dc66a34e0afc3c35cc
cath-ly <takath339@gmail.com>
2024-02-21 17:23:08 -0500

    Added onClick method for Today button

--- src/components/CalendarHeader.js
+++ src/components/CalendarHeader.js
@@ -13,12 +13,16 @@ export default function CalendarHeader() {
     setMonthIndex(monthIndex + 1);
   };
 
+  const handleReset = function () {
+    setMonthIndex(dayjs().month())
+  }
+
   const { monthIndex, setMonthIndex } = useContext(GlobalContext);
   return (
     <header className="px-4 py-2 flex items-center">
       <img src={logo} alt="calendar" className="mr-2 w-12 h-12" />
       <h1 className="mr-10 text-xl text-gray-500 font-bold">Calendar</h1>
-      <button className="border rounded py-2 px-4 mr-5">Today</button>
+      <button onClick={handleReset} className="border rounded py-2 px-4 mr-5">Today</button>
       <button onClick={handlePrevMonth}>
         <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
           chevron_left
