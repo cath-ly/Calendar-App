8d41c744bfc49ff88906cb6758c285625e893044
cath-ly <takath339@gmail.com>
2024-03-05 22:41:46 -0500

    Added states from SmallCalendar into GlobalContext

--- src/contexts/ContextWrapper.js
+++ src/contexts/ContextWrapper.js
@@ -1,11 +1,24 @@
 import dayjs from "dayjs";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import GlobalContext from "./GlobalContext";
 
 export default function ContextWrapper(props) {
   const [monthIndex, setMonthIndex] = useState(dayjs().month());
+  const [smallCalendarMonth, setSmallCalendarMonth] = useState(null);
+
+  useEffect(() => {
+    if (smallCalendarMonth !== null) {
+      setMonthIndex(smallCalendarMonth);
+    }
+  }, [smallCalendarMonth]);
   return (
-    <GlobalContext.Provider value={{ monthIndex, setMonthIndex }}>
+    <GlobalContext.Provider
+      value={{
+        monthIndex,
+        setMonthIndex,
+        smallCalendarMonth,
+        setSmallCalendarMonth,
+      }}>
       {props.children}
     </GlobalContext.Provider>
   );
