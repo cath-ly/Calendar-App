550ef5341d548f23b406ba0bb3ef5d6be08654d5
cath-ly <takath339@gmail.com>
2024-03-06 18:01:22 -0500

    Reformatted getCurrentDayClass

--- src/utils/getCurrentDayClass.js
+++ src/utils/getCurrentDayClass.js
@@ -1,7 +1,10 @@
 import dayjs from "dayjs";
 
 export default function getCurrentDayClass(day) {
-  if (day.format("DD-MM-YY") === dayjs().format("DD-MM-YY")) {
+  const format = "DD-MM-YY";
+  const presentDayJS = dayjs().format(format);
+  const currentDay = day.format(format);
+  if (presentDayJS === currentDay) {
     return "bg-blue-600 text-white rounded-full w-7";
   }
   return "";
