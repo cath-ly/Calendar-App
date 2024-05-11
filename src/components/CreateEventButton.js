417b8f09c844ae41f8061fa31bab1ffe4bc836e3
cath-ly <takath339@gmail.com>
2024-05-11 09:53:31 -0400

    Created button to close EventModel

--- src/components/CreateEventButton.js
+++ src/components/CreateEventButton.js
@@ -1,11 +1,15 @@
-import React from 'react'
-import createImg from "../assets/luffy.svg"
+import React, { useContext } from "react";
+import createImg from "../assets/luffy.svg";
+import GlobalContext from "../contexts/GlobalContext";
 
 export default function CreateEventButton() {
+  const { setShowEventModel } = useContext(GlobalContext);
   return (
-    <button className="border p-2 rounded-full flex items-center shadow-md hover:shadow-2xl">
-      <img src={createImg} alt="create_event" className="w-16 h-16"/>
+    <button
+      onClick={() => setShowEventModel(true)}
+      className="border p-2 rounded-full flex items-center shadow-md hover:shadow-2xl">
+      <img src={createImg} alt="create_event" className="w-16 h-16" />
       <span className="pl-3 pr-7"> Create</span>
     </button>
-  )
+  );
 }
