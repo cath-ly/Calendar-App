93e850228def9f1013297369f331750661b92f49
cath-ly <takath339@gmail.com>
2024-05-12 20:51:03 -0400

    Updated package json to support our mapping of bg-colors for labels

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,29 @@
+const labelClasses = ["blue", "red", "yellow", "emerald", "purple", "pink"];
+
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  purge: {
+    content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+    //Because we made a dynamic class with the label we need to add those clases
+    // to the safe list so the purge does not remove that
+    safelist: [
+      ...labelClasses.map((lbl) => `bg-${lbl}-500`),
+      ...labelClasses.map((lbl) => `bg-${lbl}-200`),
+      ...labelClasses.map((lbl) => `text-${lbl}-400`),
+    ],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Open Sans"]
+        sans: ["Open Sans"],
       },
       gridTemplateColumns: {
-        "1/5": "1fr 5fr"
-      }
+        "1/5": "1fr 5fr",
+      },
     },
   },
   variants: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/forms')],
-}
+  plugins: [require("@tailwindcss/forms")],
+};
