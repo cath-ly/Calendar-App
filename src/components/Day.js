84ffd85164105ddd23739f9ffce453bdcb1ce713
cath-ly <takath339@gmail.com>
2024-05-12 20:52:28 -0400

    Allows days to be clickable for events in the calendar

--- src/components/Day.js
+++ src/components/Day.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import getCurrentDayClass from "../utils/getCurrentDayClass";
+import GlobalContext from "../contexts/GlobalContext";
 
 //day format will bring the two digits of day DD
 //ddd format will giver the day in letters for ex. MON, want to display only the first time of the month to reduce UI clutter
 export default function Day({ day, rowIdx }) {
+  const { setSelectedDay, setShowEventModel } = useContext(GlobalContext);
   return (
     <div className="border border-gray-200 flex flex-col">
       <header className="flex flex-col items-center">
@@ -11,11 +13,16 @@ export default function Day({ day, rowIdx }) {
           <p className="text-sm mt-1">{day.format("ddd").toUpperCase()}</p>
         )}
         <p
-          className={`text-sm p-1 my-1 text-center ${getCurrentDayClass(day)}`}
-        >
+          className={`text-sm p-1 my-1 text-center ${getCurrentDayClass(day)}`}>
           {day.format("DD")}
         </p>
       </header>
+      <div
+        className="flex-1 cursor-pointer"
+        onClick={() => {
+          setSelectedDay(day);
+          setShowEventModel(true);
+        }}></div>
     </div>
   );
 }
