b66c9dba89320449cf9c3c48714eaf8d0c93a399
cath-ly <takath339@gmail.com>
2024-05-12 20:53:34 -0400

    Added more sections onto EventModel consisting of: Date, Title, Description, and Label

--- src/components/EventModel.js
+++ src/components/EventModel.js
@@ -1,8 +1,15 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import GlobalContext from "../contexts/GlobalContext";
 
+// Label colors used in Labeling new events, founded in Background Color in tailwindcss
+const labelClasses = ["blue", "red", "yellow", "emerald", "purple", "pink"];
+
 export default function EventModel() {
-  const { setShowEventModel } = useContext(GlobalContext);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState([]);
+  const [selectedLabel, setSelectedLabel] = useState(labelClasses[0]);
+  const { selectedDay, setShowEventModel } = useContext(GlobalContext);
+
   return (
     <div className="h-screen w-full fixed left-0 top-0 flex justify-center items-center">
       <form className="bg-white rounded-lg shadow-2xl w-1/4">
@@ -14,6 +21,63 @@ export default function EventModel() {
             <span className="material-icons-outlined text-gray-400">close</span>
           </button>
         </header>
+        <div className="p-3">
+          <div className="grid grid-cols-1/5 items-end gap-y-7">
+            <div></div>
+
+            <input
+              type="text"
+              name="title"
+              placeholder="Add title"
+              value={title}
+              required
+              className="pt-3 border-0 text-gray-600 text-xl font-semibold pb-2 w-full border-gray-200 focus:outline-none focus:ring-0 focus:border-blue-500"
+              onChange={(e) => setTitle(e.target.value)}
+            />
+            <span className="material-icons-outlined text-gray-400">
+              schedule
+            </span>
+            <p>{selectedDay.format("dddd, MMMM DD")}</p>
+
+            <span className="material-icons-outlined text-gray-400">
+              segment
+            </span>
+            <input
+              type="text"
+              name="description"
+              placeholder="Add description"
+              value={description}
+              required
+              className="pt-3 border-0 text-gray-600 pb-2 w-full border-gray-200 focus:outline-none focus:ring-0 focus:border-blue-500"
+              onChange={(e) => setDescription(e.target.value)}
+            />
+
+            <span className="material-icons-outlined text-gray-400">
+              bookmark_border
+            </span>
+            <div className="flex gap-x-2">
+              {labelClasses.map((label, i) => (
+                <span
+                  key={i}
+                  onClick={() => setSelectedLabel(label)}
+                  className={`bg-${label}-500 w-6 h-6 rounded-full flex items-center justify-center cursor-pointer`}>
+                  {selectedLabel === label && (
+                    <span className="material-icons-outlined text-white text-sm items-center">
+                      check
+                    </span>
+                  )}
+                </span>
+              ))}
+            </div>
+          </div>
+        </div>
+        <footer className="flex justify-end border-t p-3 mt-5">
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-600 px-6 py-2 rounded text-white">
+            Confirm
+          </button>
+        </footer>
       </form>
     </div>
   );
