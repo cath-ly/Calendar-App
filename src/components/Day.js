baf184bb3492fefe6cdcc5e73964d1c84b8bf1cd
cath-ly <takath339@gmail.com>
2024-05-29 18:03:43 -0400

    Displays event into calendar and small calendar UI

--- src/components/Day.js
+++ src/components/Day.js
@@ -1,11 +1,21 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import getCurrentDayClass from "../utils/getCurrentDayClass";
 import GlobalContext from "../contexts/GlobalContext";
+import dayjs from "dayjs";
 
 //day format will bring the two digits of day DD
 //ddd format will giver the day in letters for ex. MON, want to display only the first time of the month to reduce UI clutter
 export default function Day({ day, rowIdx }) {
-  const { setSelectedDay, setShowEventModel } = useContext(GlobalContext);
+  const [eventDay, setEventDay] = useState([]);
+  const { setSelectedDay, setShowEventModel, markedEvents } =
+    useContext(GlobalContext);
+
+  useEffect(() => {
+    const events = markedEvents.filter(
+      (event) => dayjs(event.day).format("DD-MM-YY") === day.format("DD-MM-YY")
+    );
+    setEventDay(events);
+  }, [markedEvents, day]);
   return (
     <div className="border border-gray-200 flex flex-col">
       <header className="flex flex-col items-center">
@@ -22,7 +32,15 @@ export default function Day({ day, rowIdx }) {
         onClick={() => {
           setSelectedDay(day);
           setShowEventModel(true);
-        }}></div>
+        }}>
+        {eventDay.map((event, index) => (
+          <div
+            key={index}
+            className={`bg-${event.label}-200 p-1 mr-3 text-gray-600 text-sm rounded mb-1 truncate`}>
+            {event.title}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
