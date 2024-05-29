860903758e917ded4c2fe8c0cc3b659b4a8ee083
cath-ly <takath339@gmail.com>
2024-05-29 18:02:51 -0400

    Resolved const issue with parsedEvent

--- src/utils/initEvents.js
+++ src/utils/initEvents.js
@@ -1,7 +1,7 @@
-const storedEvents = localStorage.getItem("markedEvents");
 export default function initEvents() {
-  const parsedEvents = [];
-  if (typeof storedEvents !== "undefined") {
+  const storedEvents = localStorage.getItem("markedEvents");
+  let parsedEvents = [];
+  if (storedEvents !== null) {
     parsedEvents = JSON.parse(storedEvents);
   }
   return parsedEvents;
