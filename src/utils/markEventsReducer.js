3f43ca25ff958e3d1cbcf2b479d6c5e3751ebe29
cath-ly <takath339@gmail.com>
2024-06-13 21:01:46 -0400

    Optimized by incorporating map and filter to update and delete

--- src/utils/markEventsReducer.js
+++ src/utils/markEventsReducer.js
@@ -4,25 +4,10 @@ export default function markEventsReducer(state, { type, payload }) {
       return [...state, payload];
 
     case "update":
-      const updatedState = [];
-      for (const event in state) {
-        if (event.id === payload.id) {
-          updatedState.push(payload);
-        } else {
-          updatedState.push(event);
-        }
-      }
-      return updatedState;
+      return state.map((event) => (event.id === payload.id ? payload : event));
 
     case "delete":
-      const deletedState = [];
-      //seems stupid, does a for loop to find the event itself, can we make this into a o(1) runtime
-      for (const event in state) {
-        if (event.id !== payload.id) {
-          deletedState.push(event);
-        }
-      }
-      return deletedState;
+      return state.filter((event) => event.id !== payload.id);
 
     default:
       throw new Error();
